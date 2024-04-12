0491edae8c0c21f61605e6e6ecfa576a64711982
chanjin97 <chanjindark@naver.com>
2024-04-12 15:39:59 +0900

    user / userRouter

---
+++ back/src/models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    maxLength: 50,
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: true,
+  },
+
+  password: {
+    type: String,
+    minLength: 4,
+  },
+  role: {
+    type: String,
+    default: 0,
+  },
+  image: String,
+  createdAt: Date,
+});
+
+const User = mongoose.model("user", userSchema);
+module.exports = User;
---
+++ back/src/routers/userRouter.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const userRouter = express.Router();
+const User = require("../models/User");
+const { hash, compare } = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+userRouter.post("/register", async (req, res) => {
+  try {
+    const password = await hash(req.body.password, 10);
+    const user = await new User({
+      name: req.body.name,
+      email: req.body.email,
+      password,
+      createdAt: new Date(),
+    }).save();
+    return res.status(200).send({ user });
+  } catch (error) {
+    console.log("post error");
+  }
+});
+
+userRouter.post("login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res
+        .status(400)
+        .send({ error: "이메일이 없습니다 다시 확인해주세요." });
+    }
+    const isMatch = await compare(req.body.password, user.password);
+    if (!isMatch) {
+      return res
+        .status(400)
+        .send({ error: "비밀번호가 틀립니다 다시 확인해주세요" });
+    }
+
+    const payload = {
+      userId: user._id.toHexString(),
+      email: user.email /* 찾은애(find)의 user의 eamil */,
+    };
+
+    const accessToken = jwt.sign(payload, process.env.SECRET_KEY, {
+      expiresIn: "1h",
+    });
+
+    return res.status(200).send({ user, accessToken, message: "로그인 ok" });
+  } catch (error) {}
+});
+
+module.exports = userRouter;
