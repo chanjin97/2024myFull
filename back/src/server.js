607ff80de2114810b79789be2882e2c236a5f97c
chanjin97 <chanjindark@naver.com>
2024-04-12 15:40:27 +0900

    server

---
+++ back/src/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRouter = require("./routers/userRouter");
+
+dotenv.config();
+app.use(express.json());
+
+const server = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("mongoDB connected");
+
+    mongoose.set("debug", true);
+
+    app.use("/user", userRouter);
+
+    app.listen(4000, function () {
+      console.log("server on port 4000");
+    });
+  } catch (error) {
+    console.log("연결 실패");
+  }
+};
+server();
